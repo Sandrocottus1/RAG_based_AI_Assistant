9c795e0b85340a3c821af4da12caafe6f3c1459f
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2026-02-05 13:25:06 +0530

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+
+class Cfg :
+    pg_title="Pluang AI Knowledge Base"
+    mdl_nm="sentence-transformers/all-MiniLM-L6-v2"
+    ch_sz=500
+    ch_ol=50
+    k_ret=3
+    d_path="data/raw"
+    v_path="data/vector_store"
+
+    
